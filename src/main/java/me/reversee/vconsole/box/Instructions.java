c56dfabac918a504fe39c91e2ccf5404d5c7d4e7
reversee <rare7989@gmail.com>
2022-07-19 21:28:10 +0200

    Instructions: Fix example

--- src/main/java/me/reversee/vconsole/box/Instructions.java
+++ src/main/java/me/reversee/vconsole/box/Instructions.java
@@ -3,7 +3,7 @@ package me.reversee.vconsole.box;
 public enum Instructions {
     FLG, // eg. flg 0x0A                    - sets a flag
 
-    MOV, // eg. mov PBA, "Hello, World"!    - moves value into register
+    MOV, // eg. mov PBA, "Hello, World!"    - moves value into register
     ADD, // eg. add VAA, 10                 - adds value to register
     INC, // eg. INC VAA, 2                  - increments register by value
     DEC, // eg. DEC VAA, 2                  - decrements register by value
