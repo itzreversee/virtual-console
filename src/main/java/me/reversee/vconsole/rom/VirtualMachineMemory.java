9a6b18a5170f73f11a9fe6e2c12471d3f2683224
reversee <rare7989@gmail.com>
2022-07-27 11:13:16 +0200

    VirtualMachineMemory: initialize values

--- src/main/java/me/reversee/vconsole/rom/VirtualMachineMemory.java
+++ src/main/java/me/reversee/vconsole/rom/VirtualMachineMemory.java
@@ -4,17 +4,17 @@ import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
 public class VirtualMachineMemory {
-    public static Object REGISTER_RMA;
-    public static Object REGISTER_RMB;
-    public static Object REGISTER_RMC;
-    public static Object REGISTER_RMD;
+    public static Object REGISTER_RMA = null;
+    public static Object REGISTER_RMB = null;
+    public static Object REGISTER_RMC = null;
+    public static Object REGISTER_RMD = null;
 
-    public static Object REGISTER_PBA;
-    public static Object REGISTER_PBB;
-    public static Object REGISTER_PBC;
-    public static Object REGISTER_PBD;
-    public static Object REGISTER_PBE;
-    public static Object REGISTER_PBF;
+    public static Object REGISTER_PBA = null;
+    public static Object REGISTER_PBB = null;
+    public static Object REGISTER_PBC = null;
+    public static Object REGISTER_PBD = null;
+    public static Object REGISTER_PBE = null;
+    public static Object REGISTER_PBF = null;
 
     public static ArrayList<LinkedHashMap<String, Object>> Variables;
 
