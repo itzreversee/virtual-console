7366dfebdbab9cf79ba7c4160352ea10a4b32dbb
reversee <rare7989@gmail.com>
2022-07-27 11:15:42 +0200

    Instructions: add MVA instruction (not implemented)

--- src/main/java/me/reversee/vconsole/box/Instructions.java
+++ src/main/java/me/reversee/vconsole/box/Instructions.java
@@ -4,6 +4,7 @@ public enum Instructions {
     FLG, // eg. flg 0x0A                    - sets a flag
 
     MOV, // eg. mov PBA, "Hello, World!"    - moves value into register
+    MVA, // eg. mov PBA, PBB                - moves register into another register
     ADD, // eg. add VAA, 10                 - adds value to register
     INC, // eg. INC VAA, 2                  - increments register by value
     DEC, // eg. DEC VAA, 2                  - decrements register by value
