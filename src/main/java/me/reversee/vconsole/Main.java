c08807ca12e160ea5a19d92581857975af4b79bb
reversee <46657288+reversee-dev@users.noreply.github.com>
2022-09-03 19:57:28 +0200

    Bump version to 0.5

--- src/main/java/me/reversee/vconsole/Main.java
+++ src/main/java/me/reversee/vconsole/Main.java
@@ -12,7 +12,7 @@ import java.util.LinkedHashMap;
 public class Main {
 
     public static int _version_major = 0;
-    public static int _version_minor = 4;
+    public static int _version_minor = 5;
     public static String _version_string = _version_major + "." + _version_minor;
 
     public static void main(String[] args) throws NotImplementedException {
